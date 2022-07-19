24ce926b8b9bd073884b22dcab261b683b4395e5
hongsemy <semyeong.hong@mail.utoronto.ca>
2022-07-19 17:47:02 +0900

    add static file root

--- hakcs/settings.py
+++ hakcs/settings.py
@@ -128,4 +128,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import os
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'website/static')]
